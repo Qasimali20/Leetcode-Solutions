065036a75bb6e7da6198f73f1da82cb0d86e53d7
Qasimali20 <124170100+Qasimali20@users.noreply.github.com>
2023-07-27 15:39:03 +0500

    Add files via upload

---
+++ Two Pointer/11.container_water.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l,r=0,len(height)-1
+        res=0
+        
+        while l<r:
+            area= (r-l) * min(height[l], height[r])
+            res=max(res,area)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+                
+        return res
+            
+h=[1,8,6,2,5,4,8,3,7]
+s=Solution()
+res=s.maxArea(h)
+print(res)
