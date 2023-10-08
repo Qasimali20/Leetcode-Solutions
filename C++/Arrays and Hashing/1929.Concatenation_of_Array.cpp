c1878019970b31b31fd75c52a516f5a30dbfa8bb
Qasimali20 <u2022414@giki.edu.pk>
2023-10-08 21:17:57 +0500

    Create 1929.Concatenation_of_Array.cpp

---
+++ C++/Arrays and Hashing/1929.Concatenation_of_Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector <int> ans (2*n);
+           
+        for (int i=0; i<n; i++){
+            ans[i]=ans[i+n]=nums[i];
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for (int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
